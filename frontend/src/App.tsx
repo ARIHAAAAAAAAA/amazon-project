ee3e2d9b2ef7a462ddb5b9be18a6a8a2255f4292
ARIHAAAAAAAAA <ari05050700@gmail.com>
2023-10-05 15:11:32 +0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>
+        TS amazon
+      </header>
+      <main></main>
+      <footer>
+        ALLright reserved
+      </footer>
+    </div>
+  )
+}
+
+export default App
